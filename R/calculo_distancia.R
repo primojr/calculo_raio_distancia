5add22ede660c1d7bfb26c0100e0e8159050f899
primojr <gersonprimo@gmail.com>
2022-03-03 10:14:57 -0300

    Updade sintaxe do calculo

--- R/calculo_distancia.R
+++ R/calculo_distancia.R
@@ -13,7 +13,7 @@ token <- "0f088d16c04b648813fa754a77514d2b" # Meu token
 cepR::busca_cep(cep = '46400000', token)
 
 # Carregar base de lojas
-lojas <- read_excel("data.frame/enderecos_hortfruti.xlsx", sheet = 1) %>% janitor::clean_names()
+lojas <- read_excel("date/Lojas - Endereços completos.xlsx", sheet = 1) %>% janitor::clean_names()
 
 ## Remover caracteres do CEP
 lojas <- lojas %>%
@@ -23,11 +23,14 @@ lojas <- lojas %>%
 lojas <- lojas[,-6]
 lojas %>% janitor::tabyl(qtd_caracters)
 
-clientes <- read_csv("date/userid.csv")
+clientes <- read_csv("date/userid.csv") %>%
+  mutate(latitude  = round(latitude , 5), longitude = round(longitude,5)) %>%
+  distinct(.keep_all = TRUE)
+
 glimpse(clientes)
 
 # Cep das lojas
-cep_t = lojas$CEP
+cep_t = lojas$cep
 
 # Buscar as coordenadas
 lojas_ll = as.data.frame(NULL)
@@ -37,24 +40,44 @@ for(i in 1:length(cep_t)){
   lojas_ll <- rbind(lojas_ll,a)
 }
 
-lojas_ll <- na.omit(b)
-lojas_ll
-
-# Cruzar com codigo da loja
-#b <- inner_join(b, lojas, by = c())
+lojas_ll <- na.omit(lojas_ll)
+lojas_ll$latitude
 
 
+# Codigo da loja com as coordenadas
+lojas_ll <- inner_join(lojas, select(lojas_ll,cep, latitude,longitude), by = c("cep"="cep"))
+
+lojas_ll <-  distinct(lojas_ll, .keep_all = TRUE)
+
+# Loja proxima
+# r = 1.5
+
+calcular_distancia = function(num_da_loja){
+# Codigo da loja
+  n_loja = num_da_loja
+# Latitude e longitude da loja
+  lat = rep(lojas_ll$latitude[lojas_ll$loja == n_loja],nrow(clientes))
+  lon = rep(lojas_ll$longitude[lojas_ll$loja == n_loja],nrow(clientes))
 # Calculo da distancia
+  ll_loja = purrr::map2(lat, lon, function(x,y) c(x,y))
+  ll_cliente = purrr::map2(clientes$latitude, clientes$longitude, function(x,y) c(x,y))
+  dist_loja = purrr::map2_dbl(ll_loja, ll_cliente, function(x,y) haversine(x, y))
+  dist_loja
+}
 
-# Separar a longitude da cidade da loja
-lojas_ll$lat.cabo_frio = lojas_ll$latitude[b$cidade == 'Cabo Frio']
-lojas_ll$lon.cabo_frio = lojas_ll$longitude[b$cidade == 'Cabo Frio']
+
+col <- unique(lojas_ll$loja)
+dist = purrr::map_dfc(.x = col, .f = calcular_distancia)
+colnames(dist) <- col
+
+clientes <- cbind(clientes, dist)
 
 
-longlat1 = purrr::map2(lojas_ll$lat.cabo_frio, lojas_ll$lon.cabo_frio, function(x,y) c(x,y))
-longlat2 = purrr::map2(lojas_ll$latitude, lojas_ll$longitude, function(x,y) c(x,y))
-dist_caboFrio = purrr::map2_dbl(longlat1, longlat2, function(x,y) haversine(x, y))
-b <- cbind(lojas_ll, dist_caboFrio)
+## Contagem
+clientes <- clientes %>%
+  mutate(across(H006:T814, ~ if_else(.x <= 2,1,0)))
 
+clientes %>%
+summarise(across(H006:T814, ~ sum(.x))) %>% t()
 
 
